c97847ded70b40c4e9d3ca6b6f1b8e1dcc89e8ff
Youssef Kayk <kayke8youssef@gmail.com>
2024-04-17 18:07:31 -0700

    yk

--- models/place.py
+++ models/place.py
@@ -1,4 +1,5 @@
-ce Module for HBNB project """
+#!/usr/bin/python3
+""" Place Module for HBNB project """
 from models.base_model import BaseModel
 
 
@@ -15,4 +16,3 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-
